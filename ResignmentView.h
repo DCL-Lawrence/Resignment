6f5ded5c4c3b6bcd1b4b3804e23a313718afa853
L.D.C Lin <96625924+DCL-Lawrence@users.noreply.github.com>
2024-08-01 23:22:12 +0800

    Add files via upload

---
+++ ResignmentView.h
@@ -0,0 +1,60 @@
+﻿
+// ResignmentView.h: CResignmentView 類別的介面
+//
+
+#pragma once
+
+
+class CResignmentView : public CFormView
+{
+protected: // 僅從序列化建立
+	CResignmentView() noexcept;
+	DECLARE_DYNCREATE(CResignmentView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_RESIGNMENT_FORM };
+#endif
+
+// 屬性
+public:
+	CResignmentDoc* GetDocument() const;
+
+// 作業
+public:
+
+// 覆寫
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支援
+	virtual void OnInitialUpdate(); // 建構後第一次呼叫
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+
+// 程式碼實作
+public:
+	virtual ~CResignmentView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 產生的訊息對應函式
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedAgree();
+	afx_msg void OnBnClickedDisagree();
+	unsigned int count;
+};
+
+#ifndef _DEBUG  // 對 ResignmentView.cpp 中的版本進行偵錯
+inline CResignmentDoc* CResignmentView::GetDocument() const
+   { return reinterpret_cast<CResignmentDoc*>(m_pDocument); }
+#endif
+
---
+++ resource.h
@@ -0,0 +1,23 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 產生的 Include 檔案。
+// 由 Resignment.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDD_RESIGNMENT_FORM             101
+#define IDR_MAINFRAME                   128
+#define IDR_ResignmentTYPE              130
+#define IDC_AGREE                       1000
+#define IDC_BUTTON2                     1001
+#define IDC_DISAGREE                    1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
